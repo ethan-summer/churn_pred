3f9a77722a1a72879ff1066b0a6abfb11e19964e
momentayingjie.xia <yingjie.xia@momenta.ai>
2024-04-15 20:54:16 +0800

    [main] update

--- frontend/vue-project/src/components/CsvViewer.vue
+++ frontend/vue-project/src/components/CsvViewer.vue
@@ -37,16 +37,25 @@ export default {
   },
   methods: {
     parseCSV() {
-      if (!this.file) return;
-      Papa.parse(this.file, {
-        complete: (results) => {
-          this.csvHeaders = results.data[0];
-          this.csvData = results.data.slice(1);
-        },
-        header: false
-      });
-    }
-  },
+  if (!this.file) return;
+  Papa.parse(this.file, {
+    complete: (results) => {
+      const dataIndex = results.data[0].indexOf('id'); // 查找'id'列的索引
+      if (dataIndex !== -1) {
+        // 如果找到'id'列，则从headers中移除
+        this.csvHeaders = results.data[0].filter((_, index) => index !== dataIndex);
+        // 同时从每行数据中移除'id'列
+        this.csvData = results.data.slice(1).map(row => row.filter((_, index) => index !== dataIndex));
+      } else {
+        // 如果没有'id'列，直接赋值
+        this.csvHeaders = results.data[0];
+        this.csvData = results.data.slice(1);
+      }
+    },
+    header: false
+  });
+}
+},
   mounted() {
     this.parseCSV(); // Initial parse in case the file is already set on mount
   }
